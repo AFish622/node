743b96ee1cf54f818b2b25ee110eac0b5a2a869b
Austin Fisher <AFish622@gmail.com>
2018-03-31 09:17:34 -0700

    added buttons

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -14,3 +14,9 @@ exports.createStore = async (req, res) => {
 	req.flash('success', `Successfully created ${store.name}. Would you like to leave a review?`);
 	res.redirect(`store/${store.slug}`)
 }
+
+exports.getStores = async (req, res) => {
+	//query the DB for a list of all the stores
+	const stores = await Store.find();
+	res.render('stores', {title: 'Stores', stores})
+}
